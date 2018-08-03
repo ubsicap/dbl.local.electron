f2fb13f9628bc4c6d81099a616e941d312e9f9b2
Eric Pyle <eric_pyle@sil.org>
2018-08-02 22:22:42 -0500

    add tooltip to remind what is being confirmed

--- app/components/ConfirmButton.js
+++ app/components/ConfirmButton.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import Warning from '@material-ui/icons/Warning';
 import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
 import { utilities } from '../utils/utilities';
 
 type Props = {
@@ -36,14 +37,16 @@ export default class ConfirmButton extends Component<Props> {
       classes, ...rest
     } = this.props;
     return (
-      <Button
-        {...rest}
-        color="secondary"
-        onClick={this.onClick}
-      >
-        <Warning key="btnConfirm" className={classNames(classes.leftIcon, classes.iconSmall)} />
-        Confirm
-      </Button>
+      <Tooltip title={this.props.children[1]} placement="bottom">
+        <Button
+          {...rest}
+          color="secondary"
+          onClick={this.onClick}
+        >
+          <Warning key="btnConfirm" className={classNames(classes.leftIcon, classes.iconSmall)} />
+          Confirm
+        </Button>
+      </Tooltip>
     );
   }
 
