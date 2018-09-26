37acb651e0c285ebebf275cbe575141fd5631ffe
Eric Pyle <eric_pyle@sil.org>
2018-09-26 15:28:43 -0500

    fix crash in save request when clicking Save button

--- app/reducers/bundleEditMetadata.reducer.js
+++ app/reducers/bundleEditMetadata.reducer.js
@@ -140,7 +140,7 @@ export function bundleEditMetadata(state = initialState, action) {
     }
     case bundleEditMetadataConstants.SAVE_METADATA_REQUEST: {
       const { moveNextStep: moveNext, forceSave } = action;
-      const { formKey: moveNextFormKey = null } = moveNext;
+      const { formKey: moveNextFormKey = null } = moveNext || {};
       const { currentFormWithErrors, nextFormWithErrors } = getNavigationFormsWithErrors(moveNextFormKey);
       return {
         ...state,
