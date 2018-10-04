254e0d8ba95191fe46efaeab8478eb247de9d5c5
Eric Pyle <eric_pyle@sil.org>
2018-10-04 16:45:15 -0500

    begin filling in template with workspace content

--- app/containers/WorkspacesPage.js
+++ app/containers/WorkspacesPage.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
+import { AddCircle, ImportExport, Refresh } from '@material-ui/icons';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -14,9 +13,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
+type Props = {
+  classes: {}
+};
+
 const styles = theme => ({
   appBar: {
-    position: 'relative',
+    position: 'sticky',
   },
   icon: {
     marginRight: theme.spacing.unit * 2,
@@ -62,19 +65,19 @@ const styles = theme => ({
   },
 });
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const cards = [1];
 
-function Album(props) {
+function Album(props: Props) {
   const { classes } = props;
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="static" className={classes.appBar}>
+      <AppBar className={classes.appBar}>
         <Toolbar>
-          <CameraIcon className={classes.icon} />
+          <ImportExport className={classes.icon} />
           <Typography variant="title" color="inherit" noWrap>
-            Album layout
+            nathanael
           </Typography>
         </Toolbar>
       </AppBar>
@@ -83,23 +86,24 @@ function Album(props) {
         <div className={classes.heroUnit}>
           <div className={classes.heroContent}>
             <Typography variant="display3" align="center" color="textPrimary" gutterBottom>
-              Album layout
+              Workspaces
             </Typography>
             <Typography variant="title" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
+              A workspace provides a way to associate DBL organization access tokens with their own list of DBL entries.
+              Users should create a workspace for each organization for which they have DBL roles.
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={16} justify="center">
                 <Grid item>
                   <Button variant="contained" color="primary">
-                    Main call to action
+                    <AddCircle className={classes.icon} />
+                    Create A Workspace
                   </Button>
                 </Grid>
                 <Grid item>
                   <Button variant="outlined" color="primary">
-                    Secondary action
+                    <Refresh className={classes.icon} />
+                    Refresh All
                   </Button>
                 </Grid>
               </Grid>
@@ -110,16 +114,11 @@ function Album(props) {
           {/* End hero unit */}
           <Grid container spacing={40}>
             {cards.map(card => (
-              <Grid item key={card} sm={6} md={4} lg={3}>
+              <Grid item key={card} sm={12} md={12} lg={12}>
                 <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                    title="Image title"
-                  />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="headline" component="h2">
-                      Heading
+                      WBT (IPC - LCH)
                     </Typography>
                     <Typography>
                       This is a media card. You can use this section to describe the content.
@@ -127,10 +126,10 @@ function Album(props) {
                   </CardContent>
                   <CardActions>
                     <Button size="small" color="primary">
-                      View
+                      Edit
                     </Button>
                     <Button size="small" color="primary">
-                      Edit
+                      Login
                     </Button>
                   </CardActions>
                 </Card>
@@ -142,19 +141,15 @@ function Album(props) {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="title" align="center" gutterBottom>
-          Footer
+          Summary
         </Typography>
         <Typography variant="subheading" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
+          This nathanael has {cards.length} workspace(s)
         </Typography>
       </footer>
       {/* End footer */}
     </React.Fragment>
   );
 }
-
-Album.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
 export default withStyles(styles)(Album);
