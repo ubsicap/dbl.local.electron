130eaa7de4287085dc1468b96389d2ccba91b5fb
Eric Pyle <eric_pyle@sil.org>
2019-01-25 11:24:54 -0600

    cleanup listeners after usage

--- app/actions/clipboard.actions.js
+++ app/actions/clipboard.actions.js
@@ -40,10 +40,7 @@ export function pasteItems(bundleId) {
     if (bundleId === selectedItemsToPaste.bundleId) {
       return;
     }
-    const { listeners } = clipboard;
-    if (!listeners) {
-      dispatch(setupClipboardListeners());
-    }
+    dispatch(setupClipboardListeners());
     dispatch(request(bundleId, selectedItemsToPaste.items));
     if (selectedItemsToPaste.itemsType === 'resources') {
       await bundleService.copyResources(
@@ -90,9 +87,12 @@ export function setupClipboardListeners() {
       console.error('EventSource undefined');
       return;
     }
+    const dispatchListenStorerExecuteTask = (e) => dispatch(listenStorerExecuteTask(e));
+    const dispatchListenStorerChangeMode = (e) => dispatch(listenStorerChangeMode(e));
+
     const listeners = {
-      'storer/execute_task': e => dispatch(listenStorerExecuteTask(e)),
-      'storer/change_mode': e => dispatch(listenStorerChangeMode(e)),
+      'storer/execute_task': dispatchListenStorerExecuteTask,
+      'storer/change_mode': dispatchListenStorerChangeMode,
     };
     Object.keys(listeners).forEach((evType) => {
       const handler = listeners[evType];
@@ -100,47 +100,59 @@ export function setupClipboardListeners() {
     });
     return dispatch({ type: clipboardConstants.CLIPBOARD_LISTENERS, listeners });
   };
-
-  // clipboard listenStorerExecuteTask: {"args": ["018868cf-7e57-44b0-a8d0-c8d73c5ba133", "copyMetadata"], "component": "storer", "type": "execute_task"}
-  function listenStorerExecuteTask(event) {
-    return (dispatch, getState) => {
-      const { clipboard } = getState();
-      const { selectedItemsToPaste = null, bundleIdTarget } = clipboard;
-      if (!selectedItemsToPaste || !bundleIdTarget) {
-        return;
-      }
-      const data = JSON.parse(event.data);
-      const [bundleId, task] = data.args;
-      if (bundleId !== bundleIdTarget) {
-        return;
-      }
-      console.log(`clipboard listenStorerExecuteTask: ${event.data}`);
-      if (['copyMetadata', 'copyResources'].includes(task)) {
-        dispatch({ type: clipboardConstants.CLIPBOARD_HAS_STARTED_PASTE_ITEMS, task });
-      }
-    };
-  }
-
- /* clipboard listenStorerChangeMode: {"args": ["018868cf-7e57-44b0-a8d0-c8d73c5ba133", "storeTask"], "component": "storer", "type": "change_mode"}
-  * clipboard listenStorerChangeMode: {"args": ["018868cf-7e57-44b0-a8d0-c8d73c5ba133", "store"], "component": "storer", "type": "change_mode"}
-  */
-  function listenStorerChangeMode(event) {
-    return (dispatch, getState) => {
-      const { clipboard } = getState();
-      const { selectedItemsToPaste = null, isPastingSelectedItems, bundleIdTarget } = clipboard;
-      if (!selectedItemsToPaste || !bundleIdTarget || !isPastingSelectedItems) {
-        return;
-      }
-      const data = JSON.parse(event.data);
-      const [bundleId, mode] = data.args;
-      if (bundleId !== bundleIdTarget) {
-        return;
-      }
-      console.log(`clipboard listenStorerChangeMode: ${event.data}`);
-      if (mode === 'store') {
-        dispatch({ type: clipboardConstants.CLIPBOARD_HAS_FINISHED_PASTE_ITEMS });
-      }
-    };
-  }
 }
 
+/* clipboard listenStorerExecuteTask:
+  {"args": ["018868cf-7e57-44b0-a8d0-c8d73c5ba133", "copyMetadata"],
+   "component": "storer", "type": "execute_task"}
+ */
+function listenStorerExecuteTask(event) {
+  return (dispatch, getState) => {
+    const { clipboard } = getState();
+    const { selectedItemsToPaste = null, bundleIdTarget } = clipboard;
+    if (!selectedItemsToPaste || !bundleIdTarget) {
+      return;
+    }
+    const data = JSON.parse(event.data);
+    const [bundleId, task] = data.args;
+    if (bundleId !== bundleIdTarget) {
+      return;
+    }
+    console.log(`clipboard listenStorerExecuteTask: ${event.data}`);
+    if (['copyMetadata', 'copyResources'].includes(task)) {
+      dispatch({ type: clipboardConstants.CLIPBOARD_HAS_STARTED_PASTE_ITEMS, task });
+    }
+  };
+}
+
+/* clipboard listenStorerChangeMode:
+  {"args": ["018868cf-7e57-44b0-a8d0-c8d73c5ba133", "storeTask"],
+  "component": "storer", "type": "change_mode"}
+* clipboard listenStorerChangeMode:
+  {"args": ["018868cf-7e57-44b0-a8d0-c8d73c5ba133", "store"],
+    "component": "storer", "type": "change_mode"}
+*/
+function listenStorerChangeMode(event) {
+  return (dispatch, getState) => {
+    const { clipboard } = getState();
+    const { selectedItemsToPaste = null, isPastingSelectedItems, bundleIdTarget } = clipboard;
+    if (!selectedItemsToPaste || !bundleIdTarget || !isPastingSelectedItems) {
+      return;
+    }
+    const data = JSON.parse(event.data);
+    const [bundleId, mode] = data.args;
+    if (bundleId !== bundleIdTarget) {
+      return;
+    }
+    console.log(`clipboard listenStorerChangeMode: ${event.data}`);
+    if (mode === 'store') {
+      dispatch({ type: clipboardConstants.CLIPBOARD_HAS_FINISHED_PASTE_ITEMS });
+      const { authentication: { eventSource } } = getState();
+      const { listeners } = clipboard;
+      Object.keys(listeners).forEach((evType) => {
+        const handler = listeners[evType];
+        eventSource.removeEventListener(evType, handler);
+      });
+    }
+  };
+}
