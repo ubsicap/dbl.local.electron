70f58d0d3dd0d120ff17912635ba0a1936e48deb
Eric Pyle <eric_pyle@sil.org>
2019-02-28 13:04:45 -0600

    fix more dependencies

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
 import createDebounce from 'redux-debounced';
-import createRootReducer from '../reducers';;
+import createRootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 
 const history = createHashHistory();
