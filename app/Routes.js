270977aa7c203ed4ddf9a719b85425bf5a90e792
Eric Pyle <eric_pyle@sil.org>
2019-08-13 16:23:33 -0500

    prettier

--- app/Routes.js
+++ app/Routes.js
@@ -13,26 +13,62 @@ import EntryReports from './components/EntryReports';
 export default () => (
   <App>
     <Switch>
-      <Route exact path={navigationConstants.NAVIGATION_WORKSPACES} component={WorkspacesPage} />
-      <Route exact path={navigationConstants.NAVIGATION_WORKSPACE_LOGIN} component={LoginForm} />
-      <Route exact path={navigationConstants.NAVIGATION_UNKNOWN_WORKSPACE_LOGIN} component={LoginForm} />
+      <Route
+        exact
+        path={navigationConstants.NAVIGATION_WORKSPACES}
+        component={WorkspacesPage}
+      />
+      <Route
+        exact
+        path={navigationConstants.NAVIGATION_WORKSPACE_LOGIN}
+        component={LoginForm}
+      />
+      <Route
+        exact
+        path={navigationConstants.NAVIGATION_UNKNOWN_WORKSPACE_LOGIN}
+        component={LoginForm}
+      />
       <Redirect exact from="/" to={navigationConstants.NAVIGATION_WORKSPACES} />
-      <PrivateRoute exact path={navigationConstants.NAVIGATION_BUNDLES} component={BundlesPage} />
-      <PrivateRoute exact path={navigationConstants.NAVIGATION_BUNDLE_EDIT_METADATA} component={EditMetadataDialog} />
-      <PrivateRoute exact path={navigationConstants.NAVIGATION_BUNDLE_EDIT_METADATA_SECTION} component={EditMetadataDialog} />
-      <PrivateRoute exact path={navigationConstants.NAVIGATION_BUNDLE_EDIT_METADATA_FORMKEY} component={EditMetadataDialog} />
-      <PrivateRoute exact path={navigationConstants.NAVIGATION_BUNDLE_MANAGE_RESOURCES} component={ManageBundleManifestResourcesDialog} />
-      <PrivateRoute exact path={navigationConstants.NAVIGATION_ENTRY_REPORTS} component={EntryReports} />
+      <PrivateRoute
+        exact
+        path={navigationConstants.NAVIGATION_BUNDLES}
+        component={BundlesPage}
+      />
+      <PrivateRoute
+        exact
+        path={navigationConstants.NAVIGATION_BUNDLE_EDIT_METADATA}
+        component={EditMetadataDialog}
+      />
+      <PrivateRoute
+        exact
+        path={navigationConstants.NAVIGATION_BUNDLE_EDIT_METADATA_SECTION}
+        component={EditMetadataDialog}
+      />
+      <PrivateRoute
+        exact
+        path={navigationConstants.NAVIGATION_BUNDLE_EDIT_METADATA_FORMKEY}
+        component={EditMetadataDialog}
+      />
+      <PrivateRoute
+        exact
+        path={navigationConstants.NAVIGATION_BUNDLE_MANAGE_RESOURCES}
+        component={ManageBundleManifestResourcesDialog}
+      />
+      <PrivateRoute
+        exact
+        path={navigationConstants.NAVIGATION_ENTRY_REPORTS}
+        component={EntryReports}
+      />
     </Switch>
   </App>
 );
 
+/*
 const renderMergedProps = (component, ...rest) => {
   const finalProps = Object.assign({}, ...rest);
-  return (
-    React.createElement(component, finalProps)
-  );
+  return React.createElement(component, finalProps);
 };
+
 
 const PropsRoute = ({ component, ...rest }) => (
   <Route
@@ -40,3 +76,4 @@ const PropsRoute = ({ component, ...rest }) => (
     render={routeProps => renderMergedProps(component, routeProps, rest)}
   />
 );
+*/
