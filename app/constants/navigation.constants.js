78073b5f6d1ea1cf736a62448976d96725d7d7fa
Eric Pyle <eric_pyle@sil.org>
2019-08-15 16:03:18 -0500

    prettier

--- app/constants/navigation.constants.js
+++ app/constants/navigation.constants.js
@@ -4,10 +4,13 @@ export const navigationConstants = {
   NAVIGATION_UNKNOWN_WORKSPACE_LOGIN: '/unknown/workspace/login',
   NAVIGATION_BUNDLES: '/bundles',
   NAVIGATION_BUNDLE_EDIT_METADATA: '/bundles/:bundleId/edit-metadata',
-  NAVIGATION_BUNDLE_EDIT_METADATA_SECTION: '/bundles/:bundleId/edit-metadata/:section',
-  NAVIGATION_BUNDLE_EDIT_METADATA_FORMKEY: '/bundles/:bundleId/edit-metadata:formKey',
-  NAVIGATION_BUNDLE_MANAGE_RESOURCES: '/bundles/:bundleId/manage-resources/:mode',
-  NAVIGATION_ENTRY_REPORTS: '/entries/:bundleId/reports',
+  NAVIGATION_BUNDLE_EDIT_METADATA_SECTION:
+    '/bundles/:bundleId/edit-metadata/:section',
+  NAVIGATION_BUNDLE_EDIT_METADATA_FORMKEY:
+    '/bundles/:bundleId/edit-metadata:formKey',
+  NAVIGATION_BUNDLE_MANAGE_RESOURCES:
+    '/bundles/:bundleId/manage-resources/:mode',
+  NAVIGATION_ENTRY_REPORTS: '/entries/:bundleId/reports'
 };
 
 export default navigationConstants;
