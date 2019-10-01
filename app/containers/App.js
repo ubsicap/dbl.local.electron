378ef153a7139041fba6e221ddc1818061ac5f91
Eric Pyle <eric_pyle@sil.org>
2019-09-30 22:45:35 -0500

    hookup ipc_navigate.

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,7 +1,15 @@
 // @flow
 import * as React from 'react';
 import log from 'electron-log';
+import { history } from '../store/configureStore';
+import { ipcRendererConstants } from '../constants/ipcRenderer.constants';
 import { logHelpers } from '../helpers/log.helpers';
+
+const { ipcRenderer } = require('electron');
+
+ipcRenderer.on(ipcRendererConstants.KEY_IPC_NAVIGATE, (event, url) => {
+  history.push(url);
+});
 
 logHelpers.setupRendererErrorLogs();
 log.info('UI starting...');
