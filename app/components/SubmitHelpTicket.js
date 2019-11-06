9adc4c52fa7eb84558efc49b2547053cc0cbf68b
Eric Pyle <eric_pyle@sil.org>
2019-11-06 12:09:36 -0600

    prettier eslint

--- app/components/SubmitHelpTicket.js
+++ app/components/SubmitHelpTicket.js
@@ -1,11 +1,8 @@
 import upath from 'upath';
 import Button from '@material-ui/core/Button';
-import MenuAppBar from '../components/MenuAppBar';
 import TextField from '@material-ui/core/TextField';
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import MdEditor from 'react-markdown-editor-lite';
-// import MdEditor from '../lib/index.js';
 import MarkdownIt from 'markdown-it';
 import emoji from 'markdown-it-emoji';
 import subscript from 'markdown-it-sub';
@@ -18,19 +15,12 @@ import mark from 'markdown-it-mark';
 import tasklists from 'markdown-it-task-lists';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/atom-one-light.css';
-// import 'highlight.js/styles/github.css';
-// import content from './content.js';
-import content from './SubmitHelpTicket/help-ticket-template';
+import templateContent from './SubmitHelpTicket/help-ticket-template';
 import './md-example/index.css';
-
-const MOCK_DATA = content;
+import { servicesHelpers } from '../helpers/services';
+import MenuAppBar from './MenuAppBar';
 
 export default class SubmitHelpTicket extends React.Component {
-
-  mdEditor = null;
-
-  mdParser = null;
-
   constructor(props) {
     super(props);
     this.state = {
@@ -41,47 +31,55 @@ export default class SubmitHelpTicket extends React.Component {
       html: true,
       linkify: true,
       typographer: true,
-      highlight: function (str, lang) {
+      highlight: (str, lang) => {
         if (lang && hljs.getLanguage(lang)) {
           try {
             return hljs.highlight(lang, str).value;
-          } catch (__) {}
-        };
-        return '' // use external default escaping;
+          } catch (__) {
+            // catch all?
+          }
+        }
+        return ''; // use external default escaping;
       }
     })
-    .use(emoji)
-    .use(subscript)
-    .use(superscript)
-    .use(footnote)
-    .use(deflist)
-    .use(abbreviation)
-    .use(insert)
-    .use(mark)
-    .use(tasklists, { enabled: this.taskLists });
+      .use(emoji)
+      .use(subscript)
+      .use(superscript)
+      .use(footnote)
+      .use(deflist)
+      .use(abbreviation)
+      .use(insert)
+      .use(mark)
+      .use(tasklists, { enabled: this.taskLists });
   }
 
-  handleEditorChange = ({ html, text }) => {
+  mdEditor = null;
+
+  mdParser = null;
+
+  handleEditorChange = (/* {  html, text  } */) => {
     // console.log('handleEditorChange', text);
   };
 
   handleImageUpload = (file, callback) => {
     const reader = new FileReader();
     reader.onload = () => {
-      const convertBase64UrlToBlob = (urlData) => {
-        let arr = urlData.split(','), mime = arr[0].match(/:(.*?);/)[1];
-        let bstr = atob(arr[1]);
+      /*
+      const convertBase64UrlToBlob = urlData => {
+        const arr = urlData.split(',');
+        const mime = arr[0].match(/:(.*?);/)[1];
+        const bstr = atob(arr[1]);
         let n = bstr.length;
-        let u8arr = new Uint8Array(n);
-        while (n--) {
+        const u8arr = new Uint8Array(n);
+        while (n) {
           u8arr[n] = bstr.charCodeAt(n);
+          n += 1;
         }
         return new Blob([u8arr], { type: mime });
       };
       const blob = convertBase64UrlToBlob(reader.result);
+      */
       setTimeout(() => {
-        // setTimeout 模拟oss异步上传图片;
-        // 当oss异步上传获取图片地址后，执行calback回调（参数为imageUrl字符串），即可将图片地址写入markdown;
         // C:/Users/PyleE/Pictures/audio%20listing.jpg
         // reader.result; // base64
         const u = new URL(`file:///${upath.normalize(file.path)}`);
@@ -94,12 +92,14 @@ export default class SubmitHelpTicket extends React.Component {
 
   handleGetMdValue = () => {
     if (this.mdEditor) {
+      /* eslint-disable no-alert */
       alert(this.mdEditor.getMdValue());
     }
   };
 
   handleGetHtmlValue = () => {
     if (this.mdEditor) {
+      /* eslint-disable no-alert */
       alert(this.mdEditor.getHtmlValue());
     }
   };
@@ -110,8 +110,9 @@ export default class SubmitHelpTicket extends React.Component {
     this.setState({ title: value });
   };
 
-  handleClickSendFeedback = event => {
-
+  handleClickSendFeedback = () => {
+    const currentWindow = servicesHelpers.getCurrentWindow();
+    currentWindow.close();
   };
 
   render() {
@@ -129,7 +130,14 @@ export default class SubmitHelpTicket extends React.Component {
             </Button>
           </div>
         </MenuAppBar>
-        <div className="demo-wrap" style={{ paddingRight: '20px', paddingLeft: '20px', paddingTop: '10px' }}>
+        <div
+          className="demo-wrap"
+          style={{
+            paddingRight: '20px',
+            paddingLeft: '20px',
+            paddingTop: '10px'
+          }}
+        >
           <nav className="nav">
             <TextField
               required
@@ -149,10 +157,12 @@ export default class SubmitHelpTicket extends React.Component {
           </nav>
           <div className="editor-wrap" style={{ marginTop: '30px' }}>
             <MdEditor
-              ref={node => this.mdEditor = node}
-              value={MOCK_DATA}
+              ref={node => {
+                this.mdEditor = node;
+              }}
+              value={templateContent}
               style={{ height: '500px', width: '100%' }}
-              renderHTML={(text) => this.mdParser.render(text)}
+              renderHTML={text => this.mdParser.render(text)}
               config={{
                 view: {
                   menu: true,
@@ -163,7 +173,7 @@ export default class SubmitHelpTicket extends React.Component {
                   maxRow: 5,
                   maxCol: 6
                 },
-                imageUrl: 'https://octodex.github.com/images/minion.png',
+                imageUrl: 'https://octodex.github.com/images/minion.png'
               }}
               onChange={this.handleEditorChange}
               onImageUpload={this.handleImageUpload}
@@ -187,12 +197,5 @@ export default class SubmitHelpTicket extends React.Component {
         </div>
       </React.Fragment>
     );
-  };
-};
-
-/*;
-ReactDOM.render(
-  <Demo />,
-  document.getElementById('root');
-);
-*/
+  }
+}
